b45cf48a30de0024942ef617a0b8bc612d57ccc4
Caryn Keffer <carynkeffer@gmail.com>
2021-01-20 11:35:05 -0500

    market has attributes

--- test/market_test.rb
+++ test/market_test.rb
@@ -2,11 +2,14 @@ require 'minitest/pride'
 require 'minitest/autorun'
 require './lib/vendor'
 require './lib/item'
+require './lib/market'
 
 class MarketTest < Minitest::Test
-  def test_it_exists
+  def test_it_exists_and_has_attributes
     market = Market.new("South Pearl Street Farmers Market")
 
     assert_instance_of Market, market
+    assert_equal "South Pearl Street Farmers Market", market.name
+    assert_equal [], market.vendors
   end
 end
