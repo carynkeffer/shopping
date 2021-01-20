6fdf5028686ec67f959c2cc3e4023b8be5bea47c
Caryn Keffer <carynkeffer@gmail.com>
2021-01-20 12:53:38 -0500

    add total inventory

--- lib/market.rb
+++ lib/market.rb
@@ -23,4 +23,20 @@
      end
    end
 
+   def total_inventory
+     total_inventory = {}
+
+     @vendors.each do |vendor|
+       vendor.inventory.each do |item, quantity|
+         if total_inventory[item].nil?
+          total_inventory[item] = {quantity: 0, vendors: []}
+        end
+
+         total_inventory[item][:quantity] += quantity
+         total_inventory[item][:vendors].push(vendor)
+       end
+     end
+     total_inventory
+   end
+
  end
