ac5a112e015dabb57f7e21f8da83a84b2596b8db
Caryn Keffer <carynkeffer@gmail.com>
2021-01-20 11:25:52 -0500

    check_stock returns item quantity if in stock

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -3,6 +3,8 @@ class Vendor
   attr_reader :name,
               :inventory
 
+  attr_accessor :stock
+
   def initialize(name)
     @name = name
     @inventory = Hash.new
@@ -13,6 +15,10 @@ class Vendor
   end
 
   def check_stock(item)
-    0
+    if inventory.include?(item) == false
+      0
+    else
+      inventory[item]
+    end
   end
 end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -32,5 +32,6 @@ class VendorTest < Minitest::Test
     expected = {item1 => 30}
 
     assert_equal expected, vendor.inventory
+    assert_equal 30, vendor.check_stock(item1)
   end
 end
