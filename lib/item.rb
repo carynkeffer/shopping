cda16bbfbf21ea8c8f3042da475bfb4edf701ac5
Caryn Keffer <carynkeffer@gmail.com>
2021-01-20 11:09:55 -0500

    price to f

--- lib/item.rb
+++ lib/item.rb
@@ -2,9 +2,9 @@ class Item
 
   attr_reader :name,
               :price
-              
+
   def initialize(info)
     @name = info[:name]
-    @price = info[:price]
+    @price = info[:price].split("$")[1].to_f
   end
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -4,11 +4,13 @@ require './lib/item'
 require './lib/vendor'
 
 class ItemTest < Minitest::Test
-  def test_it_exists
+  def test_it_exists_and_has_attributes
     item1 = Item.new({name: 'Peach', price: "$0.75"})
     item2 = Item.new({name: 'Tomato', price: '$0.50'})
 
     assert_instance_of Item, item1
     assert_instance_of Item, item2
+    assert_equal "Tomato", item2.name
+    assert_equal 0.5, item2.price
   end
 end
