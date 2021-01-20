551b5dcc126c11a84f7df619826d51e48354312c
Caryn Keffer <carynkeffer@gmail.com>
2021-01-20 11:12:01 -0500

    vendor has a name

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -3,9 +3,10 @@ require 'minitest/autorun'
 require './lib/vendor'
 
 class VendorTest < Minitest::Test
-  def test_it_exists
+  def test_it_exists_and_has_attributes
     vendor = Vendor.new("Rocky Mountain Fresh")
 
     assert_instance_of Vendor, vendor
+    assert_equal "Rocky Mountain Fresh", vendor.name
   end
 end
